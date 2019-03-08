6de60eff1fc97224e044e84b509db19c4f76b690
wuuzw <29149346+wuuzw@users.noreply.github.com>
2019-03-08 00:37:41 -0500

    Fix incorrect save directory

--- src/main/java/converter/downloader/Downloader.java
+++ src/main/java/converter/downloader/Downloader.java
@@ -9,15 +9,15 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class Downloader {
+    static final String SAVE_DIRECTORY = "./currencydata";
+
     private final String BASE_URL = "http://www.floatrates.com/daily/";
-    private final int CONNECTION_TIMEOUT = 1000;
-    private final int READ_TIMEOUT = 300;
 
     public void downloadFile() throws IOException {
         for (Currency currency : Currency.values()) {
             URL source = new URL(BASE_URL + currency.getCode() + ".json");
             if (((HttpURLConnection) source.openConnection()).getResponseCode() == 200) {
-                FileUtils.copyURLToFile(source, new File(String.format("E:\\Desktop\\test\\%s.json", currency.getCode())), CONNECTION_TIMEOUT, READ_TIMEOUT);
+                FileUtils.copyURLToFile(source, new File(String.format("%s/%s.json", SAVE_DIRECTORY, currency.getCode())));
             }
         }
     }
