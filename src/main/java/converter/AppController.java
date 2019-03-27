919049367530bb2e2b503485b112c0ee635f367f
wuuzw <29149346+wuuzw@users.noreply.github.com>
2019-03-26 22:56:06 -0400

    add missing IOException import

--- src/main/java/converter/AppController.java
+++ src/main/java/converter/AppController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.TextField;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class AppController {
