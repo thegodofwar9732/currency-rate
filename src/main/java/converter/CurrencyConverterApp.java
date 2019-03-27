a8f7672699dd6dfea84a41bd9fb891bc709cf0b9
Matthew <sterlinm@hotmail.com>
2019-03-26 22:16:44 -0400

    added downloadfile function

--- src/main/java/converter/CurrencyConverterApp.java
+++ src/main/java/converter/CurrencyConverterApp.java
@@ -1,10 +1,15 @@
 package converter;
 
 import javafx.application.Application;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import converter.downloader.*;
 
 public class CurrencyConverterApp extends Application {
 
@@ -17,7 +22,9 @@ public class CurrencyConverterApp extends Application {
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args) throws IOException {
+    	launch(args);    	
+    	Downloader downloader = new Downloader();
+        downloader.downloadFile();
     }
 }
