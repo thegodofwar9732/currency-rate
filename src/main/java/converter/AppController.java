79b05ab17e270bd1cfdbcf6c704642004cad93fe
wuuzw <29149346+wuuzw@users.noreply.github.com>
2019-03-26 22:46:59 -0400

    fix format

--- src/main/java/converter/AppController.java
+++ src/main/java/converter/AppController.java
@@ -41,10 +41,9 @@ public class AppController {
 
     @FXML
 
-    private void update() throws Exception
-    {
-    		Downloader download = new Downloader();
-    		download.downloadFile();
+    private void update() throws IOException {
+    	Downloader download = new Downloader();
+    	download.downloadFile();
     }
 
     @FXML
