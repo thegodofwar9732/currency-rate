35e923ccdbe23390ec470fec8e562eef9af9bfc4
Esitt <estherssitt@gmail.com>
2019-05-14 23:24:58 -0400

    Added test case for getYesterdaysRate

--- src/test/java/converter/ApiClientTest.java
+++ src/test/java/converter/ApiClientTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
@@ -89,5 +91,61 @@ public class ApiClientTest {
         expected = parser.getRate(url, "usd");
 		
 		assertEquals("failure to convert euros to Dollars", expected, actual, .01);
+	}
+	
+	@Test
+	public void testgetYesterdayRateEurosToDollars() throws Exception, ParserConfigurationException, SAXException, IOException {
+		actual = api.getYesterdayRate("eur", "usd");
+		
+		String yesterday = LocalDate.now().minusDays(1).format(DateTimeFormatter.ISO_LOCAL_DATE);
+        URL url = new URL(String.format("%shistorical-exchange-rates.html?currency_date=%s&base_currency_code=%s&format_type=xml", BASE_URL, yesterday, "eur"));
+        expected = parser.getRate(url, "usd");        
+		
+		assertEquals("failure to convert euros to Dollars", expected, actual, .01);
+	}
+	
+	@Test (expected = Exception.class)
+    public void testgetYesterdayRateDollarsToDollars() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException {
+		actual = api.getYesterdayRate("usd", "usd");
+		
+		String yesterday = LocalDate.now().minusDays(1).format(DateTimeFormatter.ISO_LOCAL_DATE);
+        URL url = new URL(String.format("%shistorical-exchange-rates.html?currency_date=%s&base_currency_code=%s&format_type=xml", BASE_URL, yesterday, "usd"));
+        expected = parser.getRate(url, "usd");        
+		
+		assertEquals("failure to convert euros to Dollars", expected, actual, .01);
+	}
+
+	@Test (expected = Exception.class)
+    public void testgetYesterdayRateDollarsToNothing() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException {
+		actual = api.getYesterdayRate("usd", "");
+		
+		String yesterday = LocalDate.now().minusDays(1).format(DateTimeFormatter.ISO_LOCAL_DATE);
+        URL url = new URL(String.format("%shistorical-exchange-rates.html?currency_date=%s&base_currency_code=%s&format_type=xml", BASE_URL, yesterday, "usd"));
+        expected = parser.getRate(url, "");        
+		
+		assertEquals("failure to convert euros to Dollars", expected, actual, .01);
+	}
+	
+	@Test (expected = Exception.class)
+    public void testgetYesterdayRateNothingToDollars() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException {
+		actual = api.getYesterdayRate("", "usd");
+		
+		String yesterday = LocalDate.now().minusDays(1).format(DateTimeFormatter.ISO_LOCAL_DATE);
+        URL url = new URL(String.format("%shistorical-exchange-rates.html?currency_date=%s&base_currency_code=%s&format_type=xml", BASE_URL, yesterday, ""));
+        expected = parser.getRate(url, "usd");        
+		
+		
+		assertEquals("failure to convert euros to Dollars", expected, actual, .01);
+	}
+	
+	@Test
+	public void testgetYesterdayRateCanadianToAustralain() throws Exception, ParserConfigurationException, SAXException, IOException {
+		actual = 2 *( api.getYesterdayRate("cad", "aud"));
+		
+		String yesterday = LocalDate.now().minusDays(1).format(DateTimeFormatter.ISO_LOCAL_DATE);
+        URL url = new URL(String.format("%shistorical-exchange-rates.html?currency_date=%s&base_currency_code=%s&format_type=xml", BASE_URL, yesterday, "cad"));
+        expected = 2*(parser.getRate(url, "aud"));    
+		
+		assertEquals("failure to convert Canadian to Australian", expected, actual, .01);
 	}
 }
